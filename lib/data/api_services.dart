4c4f3ad8494198df3563a166de4e8a23bb1144b7
Khubaib Jamal <khubaib.j18@gmail.com>
2023-12-06 01:32:15 +0500

    api delete method

--- lib/data/api_services.dart
+++ lib/data/api_services.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
+  // api get method
   Future<List<dynamic>> get(String url) async {
     final response = await http.get(Uri.parse(url));
     if (response.statusCode >= 200 || response.statusCode <= 300) {
@@ -12,6 +13,7 @@ class ApiService {
     }
   }
 
+  // api post method
   Future<dynamic> post(String url, {required Map<String, dynamic> body}) async {
     final response = await http.post(
       Uri.parse(url),
@@ -20,6 +22,16 @@ class ApiService {
     );
     if (response.statusCode >= 200 || response.statusCode < 300) {
       print("unicorn successfully added");
+      return json.decode(response.body);
+    } else {
+      throw Exception('Api error');
+    }
+  }
+
+  // api delete method
+  Future<List<dynamic>> delete(String url) async {
+    final response = await http.delete(Uri.parse(url));
+    if (response.statusCode >= 200 || response.statusCode < 300) {
       return json.decode(response.body);
     } else {
       throw Exception('Api error');
